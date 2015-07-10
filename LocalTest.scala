e2713653d520e46a177dbbf162209a6090b9a54b
Ankur Dave <ankurdave@gmail.com>
2015-07-10 14:03:39 -0700

    Fork to get more memory in SBT

--- LocalTest.scala
+++ LocalTest.scala
@@ -6,17 +6,18 @@ import org.apache.spark.graphx.impl._
 object LocalTest {
 
   def main(args: Array[String]) {
-    val num_edges = args(0).toInt
+    val file = args(0)
 
     println("Loading...")
 
+    var num_edges = 0
     val builder = new EdgePartitionBuilder[Double, Double]
-    for (i <- 1 to num_edges) {
-      val fields = readLine().split("\t")
+    for (line <- scala.io.Source.fromFile(file).getLines) {
+      num_edges += 1
+      val fields = line.split("\t")
       val srcId = fields(0).toLong
       val dstId = fields(1).toLong
       builder.add(srcId, dstId, 1.0)
-      if (i % 10000 == 0) println("Loaded %d edges".format(i))
     }
 
     val ep = builder.toEdgePartition
@@ -40,7 +41,9 @@ object LocalTest {
     }
 
     val end_time = System.nanoTime
-    println("Scanned %d edges in %f seconds".format(num_edges * iters, (end_time - start_time) / 1.0e9))
+    val dur = (end_time - start_time) / 1.0e9
+    println("Scanned %d edges in %f seconds".format(num_edges * iters, dur))
+    println("%f edges/s".format(num_edges * iters / dur))
 
     println("Sum: " + sum)
   }
--- build.sbt
+++ build.sbt
@@ -9,7 +9,7 @@ scalaVersion := "2.10.4"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.0-SNAPSHOT"
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.5.0-SNAPSHOT"
 
-// Run tests with more memory
-javaOptions in test += "-Xmx12G"
+// Run apps with more memory
+javaOptions += "-Xmx12G"
 
-fork in test := true
+fork := true
